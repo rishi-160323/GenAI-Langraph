226bb0326bf8c5e768e0ab2449fd76b05cf96109
rishi-160323 <rishisoni8871@gmail.com>
2026-03-08 09:10:47 +0530

    changes

---
+++ langchain-prompt/message_placeholder.py
@@ -0,0 +1,21 @@
+"""We use the MessagesPlaceholder to load the chat history from a file and then create a prompt for the current query."""
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# chat template
+chat_template = ChatPromptTemplate([
+    ('system','You are a helpful customer support agent'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human','{query}')
+])
+
+chat_history = []
+# load chat history
+with open('chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+
+print(chat_history)
+
+# create prompt
+prompt = chat_template.invoke({'chat_history':chat_history, 'query':'Where is my refund'})
+
+print(prompt)
---
+++ langchain-prompt/prompt_ui.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+model = ChatOpenAI()
+
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template.json')
+
+
+
+if st.button('Summarize'):
+    # here we are creating a chain by combining the prompt template and the language model. The template will format the input data and the model will generate the output based on that formatted input. Instead of calling the model directly, we are using the chain to invoke the model with the formatted input. This allows us to separate the concerns of formatting the input and generating the output, making our code more modular and easier to maintain.
+    # For more info see prompt_template.py there we have done both the things separately and here we are doing it together using the chain.
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
